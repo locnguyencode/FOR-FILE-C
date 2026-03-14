482bb6222b802049e372e8b4051a5cd59849f016
locnguyenlaptrinh <locn0920@gmail.com>
2026-03-14 20:37:59 +0700

    Create Tổng bình phương.C

---
+++ Tổng bình phương.C
@@ -0,0 +1,21 @@
+Tính tổng S(n) = 1^2 + 2^2 + 3^2 + 4^2 + ... + n^2
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main(){
+int n;
+scanf("%d",&n);
+long long s=0;
+for(int i=1;i<=n;i++){
+s+=1ll*i*i;
+}
+printf("%lld",s);
+return 0;
+}
+
+
+
+
+
+
