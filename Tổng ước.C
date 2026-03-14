0cb1eba7a737c83e53af46fddd704e60755c02d5
locnguyenlaptrinh <locn0920@gmail.com>
2026-03-14 22:16:36 +0700

    Create Tổng ước.C

---
+++ Tổng ước.C
@@ -0,0 +1,29 @@
+Tính tổng ước của số nguyên dương N. Độ phức tạp mong đợi O(sqrt(N))
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+typedef long long ll;
+typedef float f;
+typedef double d;
+int main(){
+ll n;
+scanf("%lld",&n);
+ll s=0;
+for(int i=1;i<=sqrt(n);i++){
+if(n%i==0){
+s+=i;
+if(i!=n/i){
+s+=n/i;
+}
+}
+}
+printf("%lld",s);
+return 0;
+}
+
+
+
+
+
+
